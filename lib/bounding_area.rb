8e13a2fd20f1416ca4e095353efa1d85c3b897d9
Jacinda Zhong <jacindaz@gmail.com>
2014-06-07 11:17:12 -0400

    Sets up initial classes and methods
     - uses psuedocode and blank methods to outline
       what each class needs to accomplish

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,8 @@
 class BoundingArea
+
+  # contains_point?(x, y) returns true if the given (x, y) coordinate
+  # is contained within any of the bounding boxes for this area.
+  def contains_point?(x, y)
+  end
+
 end
--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,32 @@
 class BoundingBox
+
+  # width returns the width of the box.
+  def width
+  end
+
+  # height returns the height of the box.
+  def height
+  end
+
+  # left returns the left edge of the box.
+  def left
+  end
+
+  # right returns the right edge of the box.
+  def right
+  end
+
+  # top returns the top edge of the box.
+  def top
+  end
+
+  # bottom returns the bottom edge of the box.
+  def bottom
+  end
+
+  # contains_point?(x, y) returns true if the given (x, y)
+  # coordinate is within the box.
+  def contains_point?(x,y)
+  end
+
 end
