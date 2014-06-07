3ac76726dfb52b35e46c382868beed591e82fcef
Jacinda Zhong <jacindaz@gmail.com>
2014-06-07 12:13:45 -0400

    Updates psuedocode, spelling out specific conditions
     - true if the point is contained inside any of the bounding boxes
     - false if point is outside all bounding boxes
     - false if there are no boxes

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -8,10 +8,11 @@ class BoundingArea
   # contains_point?(x, y) returns true if the given (x, y) coordinate
   # is contained within ANY of the bounding boxes for this area.
   def contains_point?(x, y)
-    #loop through array of bounding_box objects
-    #check if each object contains_point?
-    #if all contains_point? == true, return true
-    #else return false
-  end
+
+    # true if ANY inside a box
+    # false if empty
+    # false if outside ALL boxes
+
+  end #method
 
 end
