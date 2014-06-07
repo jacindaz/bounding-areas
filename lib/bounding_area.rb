c106040689c448b3fe5461381745c6edd698a7be
Jacinda Zhong <jacindaz@gmail.com>
2014-06-07 11:57:19 -0400

    Psuedocode for contains_point? method in BoundingArea class

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,8 +1,17 @@
 class BoundingArea
 
+  #an array of bounding_box objects
+  def initialize(bounding_boxes)
+    @bounding_boxes = bounding_boxes
+  end
+
   # contains_point?(x, y) returns true if the given (x, y) coordinate
-  # is contained within any of the bounding boxes for this area.
+  # is contained within ANY of the bounding boxes for this area.
   def contains_point?(x, y)
+    #loop through array of bounding_box objects
+    #check if each object contains_point?
+    #if all contains_point? == true, return true
+    #else return false
   end
 
 end
